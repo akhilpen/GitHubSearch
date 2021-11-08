ca52da29d84ce6785a8914cc15f1aa1c134f0fe9
Sai Akhil pendyala <saiakhil@buffalo.edu>
2021-11-08 01:09:27 -0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/:organization',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/:organization/:name',
+    name: 'Repo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Repo.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
